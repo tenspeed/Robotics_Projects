746de8afdb0de8f188590d0bec42e4abacd338b2
Todd Smith <tsmith86@gmail.com>
2014-02-10 15:49:01 -0800

    Added Atmel TWI example

--- 328_Code/hello_world.cpp
+++ 328_Code/hello_world.cpp
@@ -36,6 +36,47 @@ void delay_ms(uint8_t ms)
 	}
 }
 
+void TWIInit(void)
+{
+	//set SCL to 500kHz
+	TWSR = 0X00;
+	TWBR = 0X0C;
+	//enable TWI
+	TWCR = (1<<TWEN);
+}
+
+void TWIStart(void)
+{
+	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+	while ((TWCR & (1<<TWINT)) == 0);
+}
+
+void TWIStop(void)
+{
+	TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
+}
+
+void TWIWrite(uint8_t data)
+{
+	TWDR = data;
+	TWCR = (1<<TWINT)|(1<<TWEN);
+	while ((TWCR & (1<<TWINT)) == 0);
+}
+
+uint8_t TWIReadACK(void)
+{
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);
+	while ((TWCR & (1<<TWINT)) == 0);
+	return TWDR;
+}
+
+uint8_t TWIReadNACK(void)
+{
+	TWCR = (1<<TWINT)|(1<<TWEN);
+	while ((TWCR & (1<<TWINT)) == 0);
+	return TWDR;
+}
+
 int main ()
 {
 	
@@ -45,18 +86,25 @@ int main ()
 	
 	sei();
 	rs232 the_serial_port(9600, 6);
-	bool blink = true;
+	uint8_t blink = 10;
 	
 
-	while (blink == true)
+	while (blink != 0)
 	{
 		
 		PORTC &= ~(1<<PIN3); //turn LED off
 		delay_ms(10);
 		PORTC |= (1<<PIN3); //turn LED on
-		
 		delay_ms(20);
 		//the_serial_port << endl << "Hello World!" << endl;
+		blink--;
+
+		if (blink == 0)	//Take a small breather, then lets keep this party rockin.
+		{
+			PORTC &= ~(1<<PIN3);
+			delay_ms(100);
+			blink = 10;
+		}
 		
 	}
 
